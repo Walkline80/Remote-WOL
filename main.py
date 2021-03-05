e60a6550aecce2f40135e27658bff3fdf05cb891
Walkline <walkline@163.com>
2021-03-05 11:22:14 +0800

    调整配置文件和入口文件位置

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+The MIT License (MIT)
+Copyright © 2021 Walkline Wang (https://walkline.wang)
+https://gitee.com/walkline/remote-wol
+"""
+
+if __name__ == '__main__':
+	import runner
+
+	runner.start()
